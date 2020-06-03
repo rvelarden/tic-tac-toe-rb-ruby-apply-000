4751bc2b520b6898b836a7a0686c2c78dacc09be
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-03 16:20:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ def turn(board)
   index = input_to_index(user_input)
   
   if valid_move?(board, index)
-    move(board,index, current_player) 
+    move(board,index, current_player = "X") 
   else 
     turn(board) 
   end 
